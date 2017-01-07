35acec896183ccf3a650e5a915e6ddcdf2e3f2e9
cafe <cafe@marsdeMacBook-Pro.local>
2017-01-07 16:50:13 +0800

    Initial Commit

---
+++ iap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iap
+//
+//  Created by cafe on 2017/1/7.
+//  Copyright © 2017年 cafe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
